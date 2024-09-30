cd8b7be3406fd7a18d38541f889380063bafeeee
Juliette Petit <aeda6666@gmail.com>
2024-09-30 18:02:14 +0000

    end init

--- game.zig
+++ game.zig
@@ -1,6 +1,8 @@
+const std = @import("std");
+
 var Card = struct{
     color:u32,
-    hidden:bool,
+    hidden:bool=true,
 };
 var Game = struct {
     l:i32,
@@ -8,25 +10,43 @@ var Game = struct {
     cards: []const Card,
 };
 
+var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    const rand = prng.random();
+
 fn initCards(nb:i32) []Card{
     //var avail_colors = nb;
-    var nb_colors=[nb]i32;
+    var nb_colors=[nb/2]i32;
     //init to empty
+    var i: i32 = 0;
     while (i<nb) : (i+=1){
         nb_colors[i]=0;
     }
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(!gpa.deinit());
     const allocator = &gpa.allocator;
-    var avail_colors:[]i32=try allocator.alloc(i32, nb);
-    var i: i32 = 0;
-    const cards = [nb] Card;
-    while (i<nb) : (i+=1){
-        cards[i]=Card{
-            .color =
+    var avail_colors:[]i32=try allocator.alloc(i32, nb_colors);
+    while (i<nb_colors) : (i +=1){
+            avail_colors[i]=2;
         }
-    };
+    i = 0;
+    const cards = [nb] Card;
+    var d : i32 = 0;
+    while (i<nb) : (i+=1){
+        while (avail_colors[d]==0){
+            d = rand.intRangeAtMost(u8, 0, nb_colors); 
+        }
+        cards[i]=Card{
+            .color = avail_colors[d],
+        };
+    }
     defer allocator.free(avail_colors);
+}
+fn arrayTo2d (i:i32, c:i32) struct {x:32, y:i32}{
+    return .{.x=i/c,.y=@mod(i, c)};
 }
 fn init(l:i32, c: i32) u32{
     const g = Game{
@@ -34,5 +54,10 @@ fn init(l:i32, c: i32) u32{
             .c = 100,
             .cards = 50,
         };
-    return x + 5;
+    initCards(50);
+}
+
+fn gameEnd():bool
+{
+
 }
