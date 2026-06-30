53804c3fe835a1d9213d09c23874fb6cf996ab6f
Juliette Petit <aeda6666@gmail.com>
2026-06-30 22:48:48 +0200

    minimal working front

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const colorNames = [
+  "angelcat",
+  "blackbandaid",
+  "blackpill",
+  "bomb",
+  "cutter",
+  "demoncat",
+  "dva",
+  "error404",
+  "glock",
+  "jirai",
+  "purpbandaid",
+  "purpbunny",
+  "purppill",
+  "whitebunny",
+];
+
 const importObject = {
   env: {
     jsSeed: () => BigInt(Date.now()),
@@ -10,19 +27,64 @@ WebAssembly.instantiateStreaming(
 ).then((result) => {
   const wasm = result.instance.exports;
   wasm.seedRng();
+
+  let gamePtr = null;
+  let cardsPtr = null;
+  const cols = 4;
+  const rows = 4;
+
+  const displayCard = (i, color) => {
+    img = document.getElementById(`card-${i}`);
+    img.src = `res/img/${colorNames[color]}.png`;
+  };
+
+  const hideCards = () => {
+    const hiddenPtr = wasm.getHiddenCards(gamePtr);
+    const hiddenMem = new Uint8Array(wasm.memory.buffer);
+    for (let i = 0; i < cols * rows; i++) {
+      if (hiddenMem[hiddenPtr + i] === 1) {
+        const img = document.getElementById(`card-${i}`);
+        img.src = "res/img/hidden.jpg";
+      }
+    }
+  };
+
+  const onCardClick = (index, color) => {
+    match = wasm.selectCard(gamePtr, index);
+
+    displayCard(index, color);
+
+    if (!match) {
+      setTimeout(() => hideCards(), 1000);
+    }
+  };
+
   const drawBoard = () => {
-    const cols = 4;
-    const rows = 4;
-    const gamePtr = wasm.init(cols * rows);
-    const cardsPtr = wasm.getCards(gamePtr);
+    gamePtr = wasm.init(cols * rows);
+    cardsPtr = wasm.getCards(gamePtr);
     const mem = new Uint32Array(wasm.memory.buffer);
 
     const baseIndex = cardsPtr / 4;
-    for (var i = 0; i < cols * rows; i++) {
-      console.log(cardsPtr, mem[baseIndex + i]);
-    }
 
-    console.log(gamePtr);
+    const container = document.createElement("div");
+    container.id = "cardGrid";
+    container.classList.add("container");
+    document.body.appendChild(container);
+
+    for (let i = 0; i < cols * rows; i++) {
+      const color = mem[baseIndex + i];
+      console.log(cardsPtr, color);
+
+      const div = document.createElement("div");
+      container.appendChild(div);
+
+      const img = document.createElement("img");
+
+      img.src = "res/img/hidden.jpg";
+      img.id = `card-${i}`;
+      img.addEventListener("click", () => onCardClick(i, color));
+      div.appendChild(img);
+    }
   };
 
   drawBoard();
